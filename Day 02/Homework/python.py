8ba1b78c219998a8bc683ce9166d02aa59d658c7
MUMLA000 <saba.mumladze000@gmail.com>
2025-02-17 23:20:56 +0400

    level02 commit

---
+++ Day 02/Homework/python.py
@@ -0,0 +1,70 @@
+print("საშინაო დავალება (Homework)")
+
+# აქ ვპრინტავთ ყველანაირ მათემატიკურ მოქმედებას (python-ის საშუალებით)
+# Here, we print all kinds of mathematical operations (using Python)
+
+# მიმატება                      (ორ რიცხვს ამატებს)    
+print(5 + 5)
+
+# გამოკლება                    (ერთი რიცხვი მეორეს აკლებს)
+print(10 - 5)
+
+# გამრავლება                   (რიცხვებს ერთმანეთზე ამრავლებს)
+print(4 * 3)
+
+# გაყოფა (/)                   (ჩვეულებრივი გაყოფა)
+print(10 / 2)
+
+# მთლიანი გაყოფა (//)         (შედეგს მთელ ნაწილამდე ამრგვალებს)
+print(10 // 3)
+
+# ნაშთიანი გაყოფა (%)          (გაყოფის შემდეგ დარჩენილი ნაშთი)
+print(10 % 3)
+
+
+     # ცვლადები
+
+# სახელი
+
+name = "საბა "
+
+# გვარი
+
+surname = "მუმლაძე "
+
+# ასაკი
+
+age = 15
+
+# აქ ვპრინტავთ სახელს, გვარს და ასაკს
+
+print(name + surname + str(age))
+
+
+# String ტიპის ცვლადები
+name = "საბა"
+city = "თბილისი"
+skill = "სისწრაფე"
+
+# Integer ტიპის ცვლადები
+age = 15
+year = 2025
+aura = 15
+
+# Float ტიპის ცვლადები
+height = 1.75
+price = 19.99
+temperature = 36.6
+
+# ყველა ცვლადის ტერმინალში გამოტანა
+print(name)
+print(city)
+print(skill)
+
+print(age)
+print(year)
+print(aura)
+
+print(height)
+print(price)
+print(temperature)
