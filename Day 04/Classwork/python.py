1bf3095826e3239f5d1d96ec0390226b3cdda295
MUMLA000 <saba.mumladze000@gmail.com>
2025-03-09 17:22:42 +0400

    commit 4

---
+++ Day 04/Classwork/python.py
@@ -0,0 +1,57 @@
+# სავარჯიშო 1
+
+point1 = 10
+point2 = 20
+result = (point1 + point2)
+
+
+print(result)
+
+
+
+
+
+# სავარჯიშო 2
+
+# N1
+name = Saba
+
+# შეცდომა: "Saba" უნდა იყოს ბრჭყალებში, რადგან ეს არის სტრიქონი.
+name = "Saba"
+
+# N2
+print(age)
+age = 20
+
+# შეცდომა: age ცვლადი უნდა განვსაზღვროთ გამოყენებამდე.
+age = 20
+print(age)
+
+# N3
+print(message)
+message = "Hello, world!"
+
+# შეცდომა: message ცვლადი უნდა განვსაზღვროთ გამოყენებამდე.
+message = "Hello, world!"
+print(message)
+
+# N4
+total = total + 10
+
+# შეცდომა: total ცვლადი უნდა არსებობდეს, სანამ მას გამოვიყენებთ.
+total = 0
+total = total + 10
+
+
+
+
+# სავარჯიშო 3
+
+# Debbuging - არის შეცდომების გასწორება
+# Bug - არის შეცდომა
+
+
+
+
+
+
---
+++ Day 04/Homework/python.py
@@ -0,0 +1,100 @@
+# დავალება N1
+
+points = 50  # ცვლადის საწყისი მნიშვნელობა
+points = points + 20  # პირველი განახლება
+points = points - 10  # მეორე განახლება
+print(points)  # საბოლოო მნიშვნელობის დაპრინტვა
+
+##################################################################################################
+
+# დავალება N2
+
+
+# N1 (საწყისი ნომერი)
+price = 10
+# print(Price)        (აქ კომენტარში ჩავწერე, რადგან P დიდი ასოთი არის და Error-ს წერდა.)
+
+price = 10
+print(price)
+
+# N2
+number = 5
+# print("The number is: " + number)
+
+number = 5
+print("The number is: " + str(number))
+
+# N3
+# city = Tbilisi
+
+city = "Tbilisi"
+
+# N4
+# print(score)
+score = 100
+
+score = 100
+print(score)
+
+# N5
+# age = age + 5
+
+year = 0
+year = year + 5
+
+# N6
+# value = value - 5
+
+value = 0
+value = value - 5
+
+# N7
+# coin -> 20
+# print(coin)
+
+coin = 20
+print(coin)
+
+# N8
+# age = 25
+# message = "I am " + age + " years old."
+
+age = 25
+message = "I am " + str(age) + " years old."
+
+# N9
+# items = 3
+# text = "You have " + items + " new messages."
+
+items = 3
+text = "You have " + str(items) + " new messages."
+
+# N10     (ბოლო ნომერი)
+# x = "7"
+# y = 3
+# result = x + y
+
+x = 7
+y = 3
+result = x + y
+
+####################################################################################################
+
+
+# დავალება N3
+
+fav_num = 7  # ჩემი საყვარელი რიცხვი
+age = 15  # ჩემი ასაკი
+product = fav_num * age  # ამ ცვლადების ნამრავლი
+
+# დავალება N4
+
+
+a = 2
+b = 5
+
+print(a + b)
+print(b - a)
+print(a * b)
+print(b / a)
+print(b // a)
